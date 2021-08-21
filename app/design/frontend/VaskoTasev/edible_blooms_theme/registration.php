4cb3be93f3543a07f58bb870de7af06edf7fbc7e
Vasko Tasev <vasko.tasev@gmail.com>
2021-08-21 20:04:26 +0200

    Add Edible and Redolent Blooms Base Theme Files

---
+++ app/design/frontend/VaskoTasev/edible_blooms_theme/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Vasko Tasev. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/VaskoTasev/edible_blooms_theme', __DIR__);
---
+++ app/design/frontend/VaskoTasev/redolent_blooms_theme/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Vasko Tasev. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/VaskoTasev/redolent_blooms_theme', __DIR__);
